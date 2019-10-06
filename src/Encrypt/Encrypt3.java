e500ed22deb9dc069c9d912f87048332ec3a6201
Oleg Romanov <Romanov.3003@mail.ru>
2019-10-06 19:12:11 +0300

    Задание 3 и 4

---
+++ src/Encrypt/Encrypt3.java
@@ -0,0 +1,22 @@
+package Encrypt;
+
+import java.util.Scanner;
+
+public class Encrypt3 {
+
+    public static void main(String[] args) {
+        int a = 0, s = 0;
+
+        System.out.println("Введите ключ");
+        Scanner num = new Scanner(System.in);
+        int k = num.nextInt();
+        System.out.println("Введите сообщение, которое необходимо закодировать");
+
+        while (a != -1) {
+            a = num.nextInt();
+            s = (a + k + 26) % 26;
+            System.out.println(s);
+        }
+    }
+}
+
