44d923939a1076839d8b837e44a79a3a1276fdc9
Oleg Romanov <Romanov.3003@mail.ru>
2020-04-20 23:49:31 +0300

    Update Test.java
    Добавил краткие описания к тестовым методам.

--- src/semestrovka/Test.java
+++ src/semestrovka/Test.java
@@ -12,11 +12,13 @@ public class Test {
         testDivide();
     }
 
+    //Вызов всех тестов метода testMaxNum()
     public static void testMaxNum() {
         testMaxNum1();
         testMaxNum2();
     }
 
+    //Вызов всех тестов метода testDelete()
     public static void testDelete() {
         testDelete1();
         testDelete2();
@@ -25,6 +27,7 @@ public class Test {
         testDelete5();
     }
 
+    //Вызов всех тестов метода testInsert()
     public static void testInsert() {
         testInsert1();
         testInsert2();
@@ -32,6 +35,7 @@ public class Test {
 
     }
 
+    //Вызов всех тестов метода testDivide()
     public static void testDivide() {
         testDivide1();
         testDivide2();
@@ -39,11 +43,13 @@ public class Test {
         testDivide4();
     }
 
+    //Вызов всех тестов метода testNewList()
     public static void testNewList() {
         testNewList1();
         testNewList2();
     }
 
+    //Вызов всех тестов метода testMerge()
     public static void testMerge() {
         testMerge1();
         testMerge2();
@@ -51,6 +57,7 @@ public class Test {
         testMerge4();
     }
 
+    //просто проверка на поиск максимального числа одинаковых элементов
     public static void testMaxNum1() {
         int [] arr = new int[] {1,4,8,1,4,1,7,1,4};
         MyLinkedList2 list = new MyLinkedList2(arr);
@@ -62,6 +69,7 @@ public class Test {
         }
     }
 
+    //проверка в случае, если в списке все элементы уникальны
     public static void testMaxNum2() {
         int [] arr = new int[] {1,4,8,7};
         MyLinkedList2 list = new MyLinkedList2(arr);
@@ -73,6 +81,7 @@ public class Test {
         }
     }
 
+    //проверка удаления элемента, когда удаляемый элемент является последним элементов списка
     public static void testDelete2() {
         int [] arr = new int[] {1, 2, 5, 10, 15};
         int [] arrExpected = new int[] {1, 2, 5, 10};
@@ -86,6 +95,7 @@ public class Test {
         }
     }
 
+    //проверка удаления элемента, когда удаляемый элемент является головой списка
     public static void testDelete1() {
         int [] arr = new int[] {1, 2, 5, 10, 15};
         int [] arrExpected = new int[] {2, 5, 10, 15};
@@ -99,6 +109,7 @@ public class Test {
         }
     }
 
+    //проверка удаления происзвольного элемента в списке
     public static void testDelete3() {
         int [] arr = new int[] {1, 2, 5, 10, 15};
         int [] arrExpected = new int[] {1, 2, 10, 15};
@@ -112,6 +123,7 @@ public class Test {
         }
     }
 
+    //удаление нескольких элементов равных К
     public static void testDelete4() {
         int [] arr = new int[] {1, 1, 2, 5, 10, 15};
         int [] arrExpected = new int[] {2, 5, 10, 15};
@@ -125,6 +137,7 @@ public class Test {
         }
     }
 
+    //удаление нескольких элементов, которые замыкают список
     public static void testDelete5() {
         int [] arr = new int[] {1, 1, 2, 5, 10, 15, 15, 15};
         int [] arrExpected = new int[] {1, 1, 2, 5, 10};
@@ -138,6 +151,7 @@ public class Test {
         }
     }
 
+    //проверка вставки элемента, который больше или равен наибольшему элементу списка
     public static void testInsert1() {
         int [] arr = new int[] {1, 2, 3, 4, 5};
         int [] arrExpected = new int[] {1, 2, 3, 4, 5, 5};
@@ -151,6 +165,7 @@ public class Test {
         }
     }
 
+    //провера вставки элемента на произвольную позицию внутри списка
     public static void testInsert2() {
         int [] arr = new int[] {2, 3, 4, 7, 9};
         int [] arrExpected = new int[] {2, 3, 4, 5, 7, 9};
@@ -164,6 +179,7 @@ public class Test {
         }
     }
 
+    //проверка вставки элемента который меньше всех элементов списка
     public static void testInsert3() {
         int [] arr = new int[] {8, 10, 33, 446, 76};
         int [] arrExpected = new int[] {0, 8, 10, 33, 76, 446};
@@ -177,6 +193,7 @@ public class Test {
         }
     }
 
+    //проверка разделдения списка на два новых списка
     public static void testDivide1() {
         int [] arr = new int[] {0, 1, 2, 3, 6, 8, 9, 10};
         int [] arr1 = new int[] {0, 3, 6, 9};
@@ -194,6 +211,7 @@ public class Test {
         }
     }
 
+    //проверка разделдения списка на два новых списка когда массив который заполняет исходный список не отсортирован
     public static void testDivide2() {
         int [] arr = new int[] {9, 1, 3, 30, 2, 10, 8};
         int [] arr1 = new int[] {3, 9, 30};
@@ -211,6 +229,7 @@ public class Test {
         }
     }
 
+    //проверка разделения списка на два новых, когда в исходном спике отсутствуют элементы кратные 3
     public static void testDivide3() {
         int [] arr = new int[] {8, 2, 5, 14, 19, 7, 22};
         int [] arr1 = new int[] {};
@@ -228,6 +247,7 @@ public class Test {
         }
     }
 
+    //проверка разделения списка на два новых, когда в исходном спике отсутствуют элементы не кратные 3
     public static void testDivide4() {
         int [] arr = new int[] {3, 6, 33, 15, 12, 9, 21, 21};
         int [] arr1 = new int[] {3, 6, 9, 12, 15, 21, 21, 33};
@@ -245,6 +265,7 @@ public class Test {
         }
     }
 
+    //создание нового списка по правилу j * (n - j)
     public static void testNewList1() {
         int [] arr = new int[] {2, 3, 5, 7 , 9};
         int [] result = new int[] {18, 18, 21, 21, 25};
@@ -259,6 +280,7 @@ public class Test {
         }
     }
 
+    //создание нового списка по правилу j * (n - j)
     public static void testNewList2() {
         int [] arr = new int[] {9, 7, 0, 3, 2};
         int [] result = new int[] {0, 0, 9, 14, 14};
@@ -273,6 +295,7 @@ public class Test {
         }
     }
 
+    //объединение двух отсортированных списков в один большой отсортированный список
     public static void testMerge1() {
         int arrFirst[] = new int[] {0, 3, 6, 9, 12};
         int arrSecond[] = new int[] {0, 1, 2, 5, 7, 10, 15};
@@ -288,6 +311,7 @@ public class Test {
         }
     }
 
+    //объединение двух отсортированных списков содержащих одинаковые элементы в один большой отсортированный список
     public static void testMerge2() {
         int arrFirst[] = new int[] {1, 1, 1, 1, 1};
         int arrSecond[] = new int[] {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
@@ -303,6 +327,7 @@ public class Test {
         }
     }
 
+    //объединение двух отсортированных списков в случае, когда все элементы второго списка больше элементов первого списка
     public static void testMerge3() {
         int arrFirst[] = new int[] {1, 2, 3, 4, 5};
         int arrSecond[] = new int[] {6, 7, 8, 9, 10, 11, 12};
@@ -318,6 +343,7 @@ public class Test {
         }
     }
 
+    //объединение двух отсортированных списков в случае, когда все элементы первого списка больше элементов второго списка
     public static void testMerge4() {
         int arrFirst[] = new int[] {6, 7, 8, 9, 10, 11, 12};
         int arrSecond[] = new int[] {0, 2, 3, 4, 5};
@@ -332,6 +358,4 @@ public class Test {
             System.out.println("fail");
         }
     }
-
-
 }
